d612674fc2810e1212158e288020c224b3b9a1e7
sedlak <slavomir.sedlak@ringier.sk>
2019-09-01 15:26:10 +0200

    create app exception

--- app/Http/Controllers/HueController.php
+++ app/Http/Controllers/HueController.php
@@ -13,7 +13,11 @@ use App\Services\GroupHueLightsService;
 
 class HueController extends Controller
 {
-
+    /**
+     * @param GroupHueLightsService $groupHueLightsService
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \App\Exceptions\invalidArrayKeyException
+     */
     public function getAllActive(GroupHueLightsService $groupHueLightsService){
         return response()->json(['result' => $groupHueLightsService->getGroupsWithDevices()['result']]);
     }
